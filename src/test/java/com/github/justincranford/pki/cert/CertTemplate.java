f8645d647307ae9cc59c2e303fcf86fd3a9a7db7
Justin Cranford <justincranford@hotmail.com>
2023-01-05 16:46:47 -0500

    Flatten extensions

--- src/test/java/com/github/justincranford/pki/cert/CertTemplate.java
+++ src/test/java/com/github/justincranford/pki/cert/CertTemplate.java
@@ -21,38 +21,38 @@ public record CertTemplate(String name, Extensions defaultExtensions, Function<E
 	}
 
 	public static final CertTemplate PROFILE_UNLIMITED_CA = new CertTemplate("Unlimited CA",
-		ExtensionUtil.EXTENSIONS_CA_UNLIMITED,
+		ExtensionUtil.extensions(ExtensionUtil.EXTENSION_BC_UNLIMITED, ExtensionUtil.EXTENSION_KU_KEYCERTSIGN_CRLSIGN),
 		ExtensionUtil.FILTER_EXTENSIONS_LAMBDA_CA
 	);
 
 	public static final CertTemplate PROFILE_ROOT_CA_1 = new CertTemplate("Root CA",
-		ExtensionUtil.EXTENSIONS_CA_1,
+		ExtensionUtil.extensions(ExtensionUtil.EXTENSION_BC_1, ExtensionUtil.EXTENSION_KU_KEYCERTSIGN_CRLSIGN),
 		ExtensionUtil.FILTER_EXTENSIONS_LAMBDA_CA
 	);
 
 	public static final CertTemplate PROFILE_SUBORDINATE_CA_0 = new CertTemplate("Subordinate CA",
-		ExtensionUtil.EXTENSIONS_CA_0,
+		ExtensionUtil.extensions(ExtensionUtil.EXTENSION_BC_0, ExtensionUtil.EXTENSION_KU_KEYCERTSIGN_CRLSIGN),
 		ExtensionUtil.FILTER_EXTENSIONS_LAMBDA_CA
 	);
 
 	public static final CertTemplate PROFILE_HTTPS_CLIENT_SERVER = new CertTemplate("HTTPS Client Server",
-		ExtensionUtil.EXTENSIONS_HTTPS_CLIENT_SERVER,
+		ExtensionUtil.extensions(ExtensionUtil.EXTENSION_KU_DIGITALSIGNATURE, ExtensionUtil.EXTENSION_EKU_CLIENT_SERVER),
 		ExtensionUtil.FILTER_EXTENSIONS_LAMBDA_END_ENTITY
 	);
 
 	public static final CertTemplate PROFILE_HTTPS_CLIENT = new CertTemplate("HTTPS Client",
-		ExtensionUtil.EXTENSIONS_HTTPS_CLIENT,
+		ExtensionUtil.extensions(ExtensionUtil.EXTENSION_KU_DIGITALSIGNATURE, ExtensionUtil.EXTENSION_EKU_CLIENT),
 		ExtensionUtil.FILTER_EXTENSIONS_LAMBDA_END_ENTITY
 	);
 
 	public static final CertTemplate PROFILE_HTTPS_SERVER = new CertTemplate("HTTPS Server",
-		ExtensionUtil.EXTENSIONS_HTTPS_SERVER,
+		ExtensionUtil.extensions(ExtensionUtil.EXTENSION_KU_DIGITALSIGNATURE, ExtensionUtil.EXTENSION_EKU_SERVER),
 		ExtensionUtil.FILTER_EXTENSIONS_LAMBDA_END_ENTITY
 	);
 
 	public static CertTemplate createCaProfile(final String name, final int pathLenConstraint) {
 		final CertTemplate caProfile = new CertTemplate(name,
-			ExtensionUtil.caExtensions(pathLenConstraint),
+			ExtensionUtil.extensions(ExtensionUtil.bcExtension(pathLenConstraint), ExtensionUtil.EXTENSION_KU_KEYCERTSIGN_CRLSIGN),
 			ExtensionUtil.FILTER_EXTENSIONS_LAMBDA_CA
 		);
 		return caProfile;
